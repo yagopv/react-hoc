15643bc6fb01feddcbfa45c951b719802a10328d
Yago Pérez Vázquez <yagoperez@outlook.com>
2017-09-20 14:40:02 +0000

    Adding components

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default (props) => {
+  return (
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>1 Cup Pepper</li>
+        <li>1 Cup Salt</li>
+      </ul>
+    </div>
+  )
+};
